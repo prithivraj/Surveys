294198982fd1495762ed04d4c0dca0355593b9e4
Prithiv Raj <prithivraj007@gmail.com>
2018-03-11 22:46:26 +0530

    Basic UI implemented

--- app/src/main/java/com/zestworks/surveys/ListingActivity.kt
+++ app/src/main/java/com/zestworks/surveys/ListingActivity.kt
@@ -3,23 +3,38 @@ package com.zestworks.surveys
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
+import android.view.View
 import com.zestworks.surveys.di.AppComponentProvider
 import com.zestworks.surveys.viewmodels.SurveysViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_listing.*
 
 class ListingActivity : AppCompatActivity() {
 
     private lateinit var surveysViewModel: SurveysViewModel
+    private lateinit var recyclerAdapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
+
+        recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerAdapter = RecyclerAdapter()
+        recycler_view.adapter = recyclerAdapter
+
+        val pagerSnapHelper = PagerSnapHelper()
+        pagerSnapHelper.attachToRecyclerView(recycler_view)
+
         surveysViewModel = ViewModelProviders.of(this).get(SurveysViewModel::class.java)
         AppComponentProvider.instance.appComponentInstance.inject(surveysViewModel)
         surveysViewModel.load().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({
-            Log.d("mainactivity", it.toString())
+            loader.visibility = View.GONE
+            recycler_view.visibility = View.VISIBLE
+            recyclerAdapter.surveyDataList = it.toList()
+            recyclerAdapter.notifyDataSetChanged()
         })
     }
 }
---
+++ app/src/main/java/com/zestworks/surveys/RecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.zestworks.surveys
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import com.zestworks.surveys.model.SurveyData
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerHolder>() {
+
+    internal var surveyDataList: List<SurveyData> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_holder, parent, false)
+        return RecyclerHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
+        val surveyData = surveyDataList[position]
+        holder.surveyTitle.text = surveyData.title
+        holder.surveyDescription.text = surveyData.description
+        Picasso.get().load(surveyData.coverImageUrl).into(holder.surveyBackground)
+    }
+
+    override fun getItemCount(): Int {
+        return surveyDataList.size
+    }
+}
+
+class RecyclerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var surveyBackground: ImageView = itemView.findViewById(R.id.survey_bgd)
+    var surveyTitle: TextView = itemView.findViewById(R.id.survey_title)
+    var surveyDescription: TextView = itemView.findViewById(R.id.survey_description)
+}
