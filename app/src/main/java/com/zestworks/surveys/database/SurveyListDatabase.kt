a7419298ecbf63f5e7d92d8a6dec502826074166
Prithiv Raj <prithivraj007@gmail.com>
2018-03-12 10:39:20 +0530

    Typeconverter class can't be empty issue

--- app/src/main/java/com/zestworks/surveys/database/SurveyListDatabase.kt
+++ app/src/main/java/com/zestworks/surveys/database/SurveyListDatabase.kt
@@ -2,11 +2,9 @@ package com.zestworks.surveys.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import android.arch.persistence.room.TypeConverters
 import com.zestworks.surveys.model.SurveyData
 
 @Database(entities = [(SurveyData::class)], version = 1)
-@TypeConverters(Converters::class)
 abstract class SurveyListDatabase : RoomDatabase() {
     abstract fun surveyListDAO(): SurveyListDAO
 }
