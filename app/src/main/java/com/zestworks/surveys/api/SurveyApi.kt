6c6dcc38c6a90e2feb4a497d5c8773100c257ea7
Prithiv Raj <prithivraj007@gmail.com>
2018-03-14 12:00:03 +0530

    Access token moved to properties file

--- app/src/main/java/com/zestworks/surveys/api/SurveyApi.kt
+++ app/src/main/java/com/zestworks/surveys/api/SurveyApi.kt
@@ -8,5 +8,5 @@ import retrofit2.http.Query
 
 interface SurveyApi {
     @GET("/surveys.json")
-    fun getAllSurveys(@Query("access_token") accessToken: String = "d9584af77d8c0d6622e2b3c554ed520b2ae64ba0721e52daa12d6eaa5e5cdd93"): Observable<Array<SurveyData>>
+    fun getAllSurveys(@Query("access_token") accessToken: String): Observable<Array<SurveyData>>
 }
